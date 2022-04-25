f741839b7d20e9ae860d02ce79f288dbf6b29b06
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-04-25 18:52:50 -0500

    bird cleanup

--- client/src/common/components/Bird/Bird.jsx
+++ client/src/common/components/Bird/Bird.jsx
@@ -1,11 +1,10 @@
-import { Center, Box, Image, Text, useColorModeValue, Flex } from "@chakra-ui/react";
+import { Box, Image, useColorModeValue } from "@chakra-ui/react";
 
 const Bird = (props) => {
   const fallbackFilter = useColorModeValue("none", "invert(90%)");
   const { img, name } = props;
   const fallback = (
     <Image
-      //  mt='auto'
       w="80%"
       alt="bird placeholder png"
       fallbackSrc="/bird-placeholder.png"
@@ -15,18 +14,12 @@ const Bird = (props) => {
   );
 
   return (
-    <Box
-      borderWidth="2px"
-      borderRadius="xl"
-      overflow="clip"
-    >
+    <Box borderWidth="2px" borderRadius="xl" overflow="clip">
       <Image
         src={img}
-        // h="100%"
         w="100%"
         fit="cover"
-        // align="75% 25%"
-        maxH='md'
+        maxH="md"
         alt={`image of ${name}`}
         fallback={fallback}
       />
