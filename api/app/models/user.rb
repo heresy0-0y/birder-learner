32fee032e43a1b4db84080e2b1f6e4b5158d3b7d
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-05-05 16:13:12 -0500

    remove rails from this repo

--- api/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, length: { minimum: 7 }
-end
--- api/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :favorites
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  post '/auth/login', to: 'authentication#login'
-  get '/auth/verify', to: 'authentication#verify'
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
