7f465c689b92ad08024ab6e197d2439d6de98c6f
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-05-06 17:01:58 -0500

    reduce limit for initial bird query

--- client/src/common/services/birds.js
+++ client/src/common/services/birds.js
@@ -16,7 +16,7 @@ export const api = createApi({
         if (countryFromIP.error) throw countryFromIP.error;
         const countryCode = countryFromIP;
         const result = await fetchWithBQ(
-          `search?&mediaType=StillImage&taxonKey=212&limit=50&basisOfRecord=HUMAN_OBSERVATION&datasetKey=50c9509d-22c7-4a22-a47d-8c48425ef4a7&country=${countryCode}`
+          `search?&mediaType=StillImage&taxonKey=212&limit=20&basisOfRecord=HUMAN_OBSERVATION&datasetKey=50c9509d-22c7-4a22-a47d-8c48425ef4a7&country=${countryCode}`
         );
         if (result.error) throw response.error;
         return result.data ? { data: result.data } : { error: result.error };
