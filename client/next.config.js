01d09f472b8b074b958a8e313a0d77bbb6f76dd2
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-05-12 16:02:29 -0500

    dependencies cleanup

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+
+  enabled: process.env.ANALYZE === "true",
+
+})
+
+module.exports = withBundleAnalyzer({
   compress: true,
   swcMinify: true,
   compiler: {
@@ -10,4 +16,4 @@ module.exports = {
     deviceSizes: [360, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 350],
   },
-};
+});
