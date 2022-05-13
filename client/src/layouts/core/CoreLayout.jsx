8860757a5eb88b2356223223547f26ef0ca628b9
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-05-13 14:00:16 -0500

    dark mode filter slate blue

--- client/src/layouts/core/CoreLayout.jsx
+++ client/src/layouts/core/CoreLayout.jsx
@@ -4,10 +4,10 @@ import {useColorMode, Box} from '@chakra-ui/react'
 
 const CoreLayout = (props) => {
   const { colorMode } = useColorMode();
-  const opacity = { light: 0, dark: 0.4 };
+  const opacity = { light: 0, dark: 0.5 };
   return (
     <>
-     <Box w="100vw" h="100vh" bg="#242938" opacity={opacity[colorMode]}  position="fixed" zIndex="-1">
+     <Box w="100vw" h="100vh" bg="#676e95" opacity={opacity[colorMode]}  position="fixed" zIndex="-1">
           </Box>
       <Nav />
       <Container>{props.children}</Container>
