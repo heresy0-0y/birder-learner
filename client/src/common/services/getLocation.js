b82874604b0e66d71f7451dd4a23a8db75df3f5c
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-06-03 19:11:08 -0500

    ip fetch error catch

--- client/src/common/services/getLocation.js
+++ client/src/common/services/getLocation.js
@@ -1,13 +1,15 @@
 async function getCountryFromIP() {
-  const response = await fetch("https://ipapi.co/json/").catch((error) => {
-    return "US";
-  });
-  if (!response.ok) {
-    return "US";
-  } else {
-    const data = await response.json();
+  try {
+    const response = await fetch("https://ipapi.co/json/");
+    if (!response.ok) {
+      return "US";
+    } else {
+      const data = await response.json();
 
-    return data.country;
+      return data.country;
+    }
+  } catch (error) {
+    return "US";
   }
 }
 
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,12 +1,6 @@
-import { useEffect } from "react";
 import Home from "../screens/Home/Home";
-import { useRouter } from "next/router";
 
 const Index = () => {
-  const router = useRouter();
-  useEffect(() => {
-    console.log(router);
-  }, []);
   return <Home></Home>;
 };
 
