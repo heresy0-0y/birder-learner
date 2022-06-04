7cf5eda8673f89a0b83bac06da94cd8e8fd30207
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-06-03 20:46:24 -0500

    no ssg for favorites

--- client/src/pages/[user.username]/favorites.jsx
+++ client/src/pages/[user.username]/favorites.jsx
@@ -9,22 +9,22 @@ import BirdGrid from "../../screens/BirdGrid/BirdGrid";
 const Favorites = () => <BirdGrid></BirdGrid>;
 
 export default Favorites;
-export async function getStaticPaths() {
-  const store = makeStore();
-  const result = await store.dispatch(getUsers.initiate());
-  console.log(result);
-  return {
-    paths: result.data?.results.map((user) => {
-      `${user}/favorites`;
-    }),
-    fallback: true,
-  };
-}
-export const getStaticProps = wrapper.getStaticProps((store) => async () => {
-  store.dispatch(getFavorites.initiate());
+// export async function getStaticPaths() {
+// const store = makeStore();
+// const result = await store.dispatch(getUsers.initiate());
+// console.log(result);
+// return {
+// paths: result.data?.results.map((user) => {
+// `${user}/favorites`;
+// }),
+// fallback: true,
+// };
+// }
+// export const getStaticProps = wrapper.getStaticProps((store) => async () => {
+// store.dispatch(getFavorites.initiate());
 
-  await Promise.all(getRunningOperationPromises());
-  return {
-    props: {},
-  };
-});
+// await Promise.all(getRunningOperationPromises());
+// return {
+// props: {},
+// };
+// });
