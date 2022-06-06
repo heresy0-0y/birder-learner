55d39486004e5e8473d59ab98cea9c76cf9edc3a
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-06-06 17:36:47 -0500

    geoapify calls in api, key is secret

--- client/src/common/services/autosuggest.js
+++ client/src/common/services/autosuggest.js
@@ -1,30 +1,28 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const key = process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY;
-
 export const suggestApi = createApi({
   reducerPath: "suggestApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://api.geoapify.com/v1/`,
+    baseUrl: `/api?`,
   }),
   keepUnusedDataFor: 31536000,
   endpoints: (builder) => ({
     getCoords: builder.query({
-      query: (location) => `geocode/search?apiKey=${key}&text=${location}`,
+      query: (location) => `geocode/search?text=${location}`,
     }),
     getSuggestions: builder.query({
       query: (text) =>
-        `geocode/autocomplete?apiKey=${key}&text=${text}&type=locality&format=json`,
+        `geocode/autocomplete?text=${text}&type=locality&format=json`,
     }),
     getLocationFromCoords: builder.query({
       query: (points) => ({
-        url: `batch/geocode/reverse?apiKey=${key}&type=city`,
+        url: `batch/geocode/reverse?type=city`,
         method: "POST",
         body: points,
       }),
     }),
     getLocationsFromQuery: builder.query({
-      query: (id) => `batch/geocode/reverse?apiKey=${key}&format=json&id=${id}`,
+      query: (id) => `batch/geocode/reverse?format=json&id=${id}`,
     }),
   }),
 });
--- client/src/pages/api/index.js
+++ client/src/pages/api/index.js
@@ -1,47 +1,27 @@
-import axios, { AxiosRequestConfig } from "axios";
-const filter = (raw, unallowed) => {
-  return Object.keys(raw)
-    .filter((key) => !unallowed.includes(key))
-    .reduce((obj, key) => {
-      obj[key] = raw[key];
-      return obj;
-    }, {});
-};
+import axios from "axios";
+const BASE_URL = `https://api.geoapify.com/v1/`;
+const KEY = process.env.GEOAPIFY_API_KEY;
 
-const handler = async (req, res) => {
-  const { endpoint } = req.query;
-  console.log(endpoint);
-  if (endpoint == null || endpoint == "") {
-    const err = {
-      error: "Missing endpoint param",
-    };
-    res.status(400).send(err);
-    return;
-  }
+export default async function handler(req, res) {
+  const method = req.method;
+  const url = req.url.slice(6);
+  const body = req.body;
+  const reqConfig = {};
 
-  if (!isNaN(parseInt(endpoint)) || typeof endpoint !== "string") {
-    const err = {
-      error: "Endpoint param must be a string",
-    };
-    res.status(400).send(err);
-    return;
+  if (method === "POST") {
+    reqConfig.method = "POST";
+    reqConfig.data = body;
+    reqConfig.url = BASE_URL + url + `&apiKey=${KEY}`;
+  } else {
+    reqConfig.method = "GET";
+    reqConfig.url = BASE_URL + url + `&apiKey=${KEY}`;
   }
 
   try {
-    const reqConfig = {
-      url: endpoint,
-      method: "get",
-      headers: { "X-Requested-With": "XMLHttpRequest" },
-      params: filter({ ...req.query }, ["endpoint"]),
-    };
-    console.log(reqConfig.params);
-    const endpointReq = await axios(reqConfig);
-    const endpointRes = await endpointReq.data;
-
-    res.status(200).send(endpointRes);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
+    const response = await axios(reqConfig);
+    res.status(200).send(response.data);
+  } catch (error) {
+    res.status(500).send(error);
+    console.log(error);
   }
-};
-export default handler;
+}
