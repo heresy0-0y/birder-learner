7fce20a84ae3cab91381d777e2ba60d7bfc2227e
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-06-06 18:19:59 -0500

    timeout

--- client/src/pages/api/index.js
+++ client/src/pages/api/index.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
   const body = req.body;
   const reqConfig = {};
   reqConfig.url = `${BASE_URL}${url}&apiKey=${KEY}`;
+  reqConfig.timeout = 10000;
 
   if (method === "POST") {
     reqConfig.method = "POST";
